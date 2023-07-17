03f47e78b6febfcf5942902a5cdc9e33600c34b7
VaroLim <trabajosydemascosas@gmail.com>
2023-07-17 19:53:04 +0200

    feat: Initialization of the rentACar project, installation of dependencies and creation of the Index file

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import connectToDb from './src/services/db.js'
+import foodRouter from './src/router/food.js'
+import authRouter from './src/router/auth.js'
+import dotenv from 'dotenv'
+import { ensureAuthenticated } from './src/middleware/auth.js'
+import cors from 'cors'
+
+dotenv.config()
+
+const startApp = async () => {
+  const app = express()
+  const port = 8080
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(cors())
+
+  app.get('/', (request, response) => {
+    response.json({ info: 'Hola mundo' })
+  })
+
+  app.use(ensureAuthenticated)
+
+  app.use('/foods', foodRouter)
+  app.use('/auth', authRouter)
+
+  try {
+    await connectToDb()
+    app.listen(port, () => {
+      console.log(`Server start in ${port} port`)
+    })
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
+
+startApp()
